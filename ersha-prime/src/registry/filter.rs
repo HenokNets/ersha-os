1037f089ebda6e2d0bf362e5e702be51e0ae5917
omer-biz <omerabdi@protonmail.com>
2026-01-06 20:33:16 +0300

    feat: add device/dispatcher registries

---
+++ ersha-prime/src/registry/filter.rs
@@ -0,0 +1,48 @@
+#[allow(dead_code)]
+use ersha_core::{DeviceId, DeviceKind, DeviceState, DispatcherState, H3Cell};
+
+use jiff;
+use std::collections::HashSet;
+
+pub enum SortBy {
+    Id,
+    ProvisionAt,
+    Location,
+    Manufacturer,
+    State,
+}
+
+pub enum SortOrder {
+    Asc,
+    Desc,
+}
+
+pub struct Pagination {
+    pub offset: usize,
+    pub limit: Option<usize>,
+}
+
+pub struct QueryOptions<F> {
+    pub filter: F,
+    pub sort_by: Option<SortBy>,
+    pub sort_order: SortOrder,
+    pub pagination: Pagination,
+}
+
+#[derive(Default)]
+pub struct DeviceFilter {
+    pub ids: Option<HashSet<DeviceId>>,
+    pub states: Option<HashSet<DeviceState>>,
+    pub kinds: Option<HashSet<DeviceKind>>,
+    pub locations: Option<HashSet<H3Cell>>,
+    pub provisioned_after: Option<jiff::Timestamp>,
+    pub provisioned_before: Option<jiff::Timestamp>,
+    pub min_sensor_count: Option<usize>,
+    pub max_sensor_count: Option<usize>,
+    pub manufacturer_pattern: Option<String>,
+}
+
+pub struct DispatcherFilter {
+    pub states: Option<HashSet<DispatcherState>>,
+    pub locations: Option<HashSet<H3Cell>>,
+}
---
+++ ersha-prime/src/registry/mod.rs
@@ -0,0 +1,33 @@
+mod filter;
+
+use ersha_core::{Device, DeviceId, Dispatcher, DispatcherId};
+use filter::{DeviceFilter, DispatcherFilter, QueryOptions};
+
+trait DeviceRegistry {
+    type Error;
+
+    async fn register(&mut self, device: Device) -> Result<(), Self::Error>;
+    async fn get(&self, id: DeviceId) -> Result<Option<Device>, Self::Error>;
+    async fn update(&mut self, id: DeviceId, new: Device) -> Result<(), Self::Error>;
+    async fn suspend(&mut self, id: DeviceId) -> Result<(), Self::Error>;
+
+    async fn batch_register(&mut self, devices: Vec<Device>) -> Result<(), Self::Error>;
+    async fn count(&self, filter: Option<DeviceFilter>) -> Result<usize, Self::Error>;
+    async fn list(&self, options: QueryOptions<DeviceFilter>) -> Result<Vec<Device>, Self::Error>;
+}
+
+trait DispatcherRegistry {
+    type Error;
+
+    async fn register(&mut self, dispatcher: Dispatcher) -> Result<(), Self::Error>;
+    async fn get(&self, id: DispatcherId) -> Result<Option<Dispatcher>, Self::Error>;
+    async fn update(&mut self, id: DispatcherId, new: Dispatcher) -> Result<(), Self::Error>;
+    async fn suspend(&mut self, id: DispatcherId) -> Result<(), Self::Error>;
+
+    async fn batch_register(&mut self, devices: Vec<Dispatcher>) -> Result<(), Self::Error>;
+    async fn count(&self, filter: Option<DispatcherFilter>) -> Result<usize, Self::Error>;
+    async fn list(
+        &self,
+        options: QueryOptions<DispatcherFilter>,
+    ) -> Result<Vec<Dispatcher>, Self::Error>;
+}
