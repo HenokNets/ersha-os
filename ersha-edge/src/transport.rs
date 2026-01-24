43002f19fd953af181d35ec213d3d432b51abd9d
omer-biz <omerabdi@protonmail.com>
2026-01-24 20:10:09 +0300

    refactor: change transport to message based communication

--- ersha-edge/src/transport.rs
+++ ersha-edge/src/transport.rs
@@ -1,28 +1,42 @@
 use crate::DeviceId;
 use crate::Error;
 use crate::ReadingPacket;
-use crate::SensorCapability;
 
 use embassy_net::{
     IpAddress, IpEndpoint, Stack,
     tcp::{State, TcpSocket},
 };
+use serde::Deserialize;
+use serde::Serialize;
 
 const SERVER_ADDR: IpEndpoint = IpEndpoint {
     addr: IpAddress::v4(10, 46, 238, 14),
     port: 9001,
 };
 
+pub const PACKET_PREAMBLE: u16 = 0xE45A;
+pub const PROTOCOL_VERSION: u8 = 0x01;
+pub const MAX_PACKET_SIZE: usize = 128;
+pub const PREAMBLE_SIZE: usize = 2;
+pub const PACKET_HEADER_SIZE: usize = 6;
+pub const MAX_PAYLOAD_SIZE: usize = MAX_PACKET_SIZE - PREAMBLE_SIZE - PACKET_HEADER_SIZE;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MsgType {
+    Reading,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Msg<'a> {
+    pub preamble: u16,
+    pub version: u8,
+    pub msg_type: MsgType,
+    pub payload: &'a [u8],
+}
+
 pub trait Transport {
     /// Called once after network join / connect
     fn provision(&mut self) -> impl Future<Output = Result<DeviceId, Error>>;
-
-    /// Send device capabilities to the server / network
-    fn announce_sensors(
-        &mut self,
-        device_id: DeviceId,
-        sensors: &[SensorCapability],
-    ) -> impl Future<Output = Result<(), Error>>;
 
     /// Send a single sensor reading
     fn send_reading(&mut self, packet: &ReadingPacket) -> impl Future<Output = Result<(), Error>>;
@@ -66,31 +80,23 @@ impl<'a> Transport for Wifi<'a> {
         Ok(id)
     }
 
-    async fn announce_sensors(
-        &mut self,
-        device_id: DeviceId,
-        sensors: &[SensorCapability],
-    ) -> Result<(), Error> {
-        let mut buf = [0u8; 64];
-        let used = postcard::to_slice(&(device_id, sensors), &mut buf)
-            .map_err(|_| Error::SerializationFailed)?;
-
-        self.socket
-            .write(used)
-            .await
-            .map_err(|_| Error::UnableToSend)?;
-        Ok(())
-    }
-
     async fn send_reading(&mut self, packet: &ReadingPacket) -> Result<(), Error> {
-        let mut buf = [0u8; 64];
-        let used = postcard::to_slice(packet, &mut buf).map_err(|_| Error::SerializationFailed)?;
+        let mut payload_buf = [0u8; MAX_PAYLOAD_SIZE];
+        let payload =
+            postcard::to_slice(packet, &mut payload_buf).map_err(|_| Error::SerializationFailed)?;
 
-        self.socket
-            .write(used)
-            .await
-            .map_err(|_| Error::UnableToSend)?;
-        Ok(())
+        let msg = Msg {
+            preamble: PACKET_PREAMBLE,
+            version: PROTOCOL_VERSION,
+            msg_type: MsgType::Reading,
+            payload,
+        };
+
+        let mut msg_buf = [0u8; MAX_PACKET_SIZE];
+        let used =
+            postcard::to_slice(&msg, &mut msg_buf).map_err(|_| Error::SerializationFailed)?;
+
+        write_all(&mut self.socket, used).await
     }
 }
 
@@ -103,6 +109,14 @@ async fn read_exact(socket: &mut TcpSocket<'_>, mut buf: &mut [u8]) -> Result<()
         }
 
         buf = &mut buf[n..];
+    }
+    Ok(())
+}
+
+async fn write_all(socket: &mut TcpSocket<'_>, mut buf: &[u8]) -> Result<(), Error> {
+    while !buf.is_empty() {
+        let n = socket.write(buf).await.map_err(|_| Error::UnableToSend)?;
+        buf = &buf[n..];
     }
     Ok(())
 }
